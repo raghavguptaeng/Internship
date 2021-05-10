e80df73252cf94a7b7a7a4199a1659c63705cd4c
raghav gupta <raghavguptaeng@users.noreply.github.com>
2021-05-10 20:38:32 +0530

    Analytics made and feature heading implemented

--- lib/screens/Body.dart
+++ lib/screens/Body.dart
@@ -1,3 +1,4 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:internship/constants.dart';
 import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
@@ -11,6 +12,9 @@ class Body extends StatefulWidget {
 
 class _BodyState extends State<Body> {
   int progressValue = 70;
+  var analytics = [
+    ['User visit',50],['New user',52]
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,13 +28,65 @@ class _BodyState extends State<Body> {
           TopInfoBar(),
           SizedBox(height: 25,),
           ProgressContainer(progressValue: progressValue),
-          SizedBox(height: 25,),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("Analytis",style: kHeadStyle.copyWith(color: Color(0xFF7B7E96),fontSize: 30),),
-          )
+          Headings(name: "Analytics",),
+          Analytics(context),
+          Headings(name: "Features",)
         ],
       ),
+      // bottomNavigationBar: AnimatedBottomNavigationBar(
+      //   icons: iconList,
+      //   activeIndex: _bottomNavIndex,
+      //   gapLocation: GapLocation.end,
+      //   notchSmoothness: NotchSmoothness.defaultEdge,
+      //   onTap: (index) => setState(() => _bottomNavIndex = index),
+      //   //other params
+      // ),
+    );
+  }
+
+  SizedBox Analytics(BuildContext context) {
+    return SizedBox(
+          height:MediaQuery.of(context).size.height*0.10,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemCount: analytics.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context,index){
+                return Container(
+                  margin: EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width*0.70,
+                  decoration: BoxDecoration(
+                    color: boxColor,
+                    borderRadius: BorderRadius.circular(25)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 30,right: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(analytics[index][0],style: kHeadStyle,),
+                        Text(analytics[index][1].toString(),style: kHeadStyle.copyWith(color: Color(0xffc4c4c4)),)
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+  }
+}
+
+class Headings extends StatelessWidget {
+  String name;
+  Headings({this.name});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.07,top: 15,bottom: 15),
+      child: Text(name,style: kHeadStyle.copyWith(color: Color(0xFF7B7E96),fontSize: 30),),
     );
   }
 }
